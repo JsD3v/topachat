41aba4eaa61012cd7b4f1fcd768f76f0d7184b79
JsD3v <jean_seb68@outlook.com>
2022-05-02 17:03:02 +0200

    ajout du back-office + login/register

---
+++ src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+
+            TextField::new('nom'),
+            EmailField::new('email'),
+            BooleanField::new('is_verified')
+
+        ];
+    }
+
+}
